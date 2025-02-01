0b1d97c59d6278753363cec5d0850d29c1d5402d
DimasNur30 <cahayamoch@gmail.com>
2025-02-01 14:27:26 +0700

    LanjutCoding

--- uas/DASHBOARD_UAS/app/urls.py
+++ uas/DASHBOARD_UAS/app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.dashboard1, name='dashboard1')
+  path('', views.dashboard1, name='dashboard1'),
+  path('statistik_harian/', views.statistik_harian, name='statistik_harian'),
 ]
--- uas/DASHBOARD_UAS/app/views.py
+++ uas/DASHBOARD_UAS/app/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def dashboard1(request):
   return render(request, 'dashboard1.html')
+
+def statistik_harian(request):
+  return render(request, 'statistik_harian.html')
